1b9e1de046a5d4846cbad7d6e60e4f7eec95298f
jeetch <jeetch96@gmail.com>
2023-04-20 20:34:42 +0530

    added blobs

--- components/Background.tsx
+++ components/Background.tsx
@@ -4,15 +4,11 @@ interface BackgroundProps {}
 
 const Background: FC<BackgroundProps> = ({}) => {
   return (
-    <div className="absolute top-0 left-0 -z-10 h-full w-full flex justify-left">
-      <div
-        className="absolute w-72 h-72 bg-indigo-900 rounded-full 
-      transform -translate-y-4 -translate-x-24 opacity-20 filter blur-xl animate-blob"
-      ></div>
-      <div
-        className="absolute w-72 h-72 bg-red-900
-      rounded-full transform -translate-y-4 translate-x-24 opacity-20 filter blur-xl animate-blob animation-delay-4000"
-      ></div>
+    <div className="absolute top-0 left-0 -z-10 h-full w-full flex justify-center">
+      <div className="relative h-full w-full">
+        <div className="absolute top-0 left-0 w-64 h-64 bg-purple-900 rounded-full filter blur-xl opacity-20 animate-blob "></div>
+        <div className="absolute top-0 left-1/4 w-64 h-64 bg-green-900 rounded-full   filter blur-xl opacity-20 animate-blob animation-delay-2000"></div>
+      </div>
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -16,31 +16,22 @@ module.exports = {
           "0%": {
             transform: "translate(0, 0) scale(1)",
           },
-          "15%": {
-            transform: "translate(10px, -20px) scale(1.05)",
+          "25%": {
+            transform: "translate(0px, -25px) scale(1.1)",
           },
-          "30%": {
-            transform: "translate(30px, -50px) scale(1.1)",
-          },
-          "45%": {
-            transform: "translate(10px, -20px) scale(1.05)",
-          },
-          "60%": {
-            transform: "translate(-20px, 20px) scale(0.9)",
+          "50%": {
+            transform: "translate(-10px, -20px) scale(1.05)",
           },
           "75%": {
             transform: "translate(-10px, 10px) scale(0.95)",
           },
-          "90%": {
-            transform: "translate(0, 0) scale(1)",
-          },
           "100%": {
             transform: "translate(0, 0) scale(1)",
-          },
+          }
         },
       },
       animation: {
-        blob: "blob 7s infinite",
+        blob: "blob 5s infinite",
       },
     },
   },
