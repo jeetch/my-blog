1f380a224e5fdb30d45180170d06a0f072fa0b75
jeetch <jeetch96@gmail.com>
2023-04-20 20:36:34 +0530

    added blobs 3

--- components/Background.tsx
+++ components/Background.tsx
@@ -6,8 +6,8 @@ const Background: FC<BackgroundProps> = ({}) => {
   return (
     <div className="absolute top-0 left-0 -z-10 h-full w-full flex justify-center">
       <div className="relative h-full w-full">
-        <div className="absolute top-0 left-0 w-64 h-64 bg-purple-900 rounded-full filter blur-xl opacity-20 animate-blob "></div>
-        <div className="absolute top-0 left-1/4 w-64 h-64 bg-green-900 rounded-full   filter blur-xl opacity-20 animate-blob animation-delay-2000"></div>
+        <div className="absolute top-0 left-0 w-64 h-64 bg-purple-900 rounded-full filter blur-xl opacity-30 animate-blob "></div>
+        <div className="absolute top-0 left-1/4 w-64 h-64 bg-green-900 rounded-full   filter blur-xl opacity-30 animate-blob animation-delay-2000"></div>
       </div>
     </div>
   );
